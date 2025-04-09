24ecdd95e3569c810cb41efdbb6fb133934b0a7d
edhubbell <ed@gsdware.com>
2025-04-09 16:31:53 -0400

    minor

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
   trailingSlash: false,
-  
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'EdHubbell', // Usually your GitHub org/user name.
