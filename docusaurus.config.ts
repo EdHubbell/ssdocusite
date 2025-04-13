80585455d142eef3c2df34c3a02672de765d94e3
edhubbell <ed@gsdware.com>
2025-04-13 16:04:28 -0400

    add models and get rid of some tags

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/stomastrap-social-card.jpg',
     navbar: {
       title: 'StomaStrap',
       logo: {
