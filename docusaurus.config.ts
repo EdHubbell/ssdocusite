713a1e63f5cf7172cb2dbed7ef8f2206a8d2e889
edhubbell <ed@gsdware.com>
2025-04-16 09:00:50 -0400

    case sensitive

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -138,7 +138,7 @@ const config: Config = {
           items: [
             {
               label: 'Info',
-              to: '/docs/info',
+              to: '/docs/Info',
             },
           ],
         },
